068022d7013633f1a5eb447c37d0ef0527a52cbf
Nahom <zerihunnahom@gmail.com>
2023-05-22 16:43:03 +0300

    Create menu displayer class

--- main_menu.rb
+++ main_menu.rb
@@ -22,16 +22,16 @@ class MainMenu
     end
   end
 
-  def display_menu
-    puts 'Please select an option:'
-    puts '1. List all books'
-    puts '2. List all people'
-    puts '3. Create a person'
-    puts '4. Create a book'
-    puts '5. Create a rental'
-    puts '6. List all rentals for a given person id'
-    puts '7. Exit'
-  end
+  # def display_menu
+  #   puts 'Please select an option:'
+  #   puts '1. List all books'
+  #   puts '2. List all people'
+  #   puts '3. Create a person'
+  #   puts '4. Create a book'
+  #   puts '5. Create a rental'
+  #   puts '6. List all rentals for a given person id'
+  #   puts '7. Exit'
+  # end
 
   def list_books(app)
     app.list_books
