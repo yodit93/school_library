cc5ec8733fb09661ae83b64dce00ee57bb16f146
Nahom <zerihunnahom@gmail.com>
2023-05-22 17:08:43 +0300

    Update main function

--- main.rb
+++ main.rb
@@ -1,10 +1,14 @@
 require_relative 'app'
 require_relative 'main_menu'
+require_relative 'menu_displayer'
+
 def main
   app = App.new
   main_menu = MainMenu.new(app)
+  displayer = MenuDisplayer.new
+
   loop do
-    main_menu.display_menu
+    displayer.display
     choice = gets.chomp.to_i
     main_menu.user_options(choice)
   end
