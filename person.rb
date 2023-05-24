68768b0fcbe6a1bd7657cf3ab7aa2772502c2c73
yodit93 <yoditabebe9391@gmail.com>
2023-05-24 16:08:13 +0300

    Update book.rb and person.rb for attribute readers

--- person.rb
+++ person.rb
@@ -2,11 +2,10 @@ require_relative 'nameable'
 require_relative 'trimmer_decorator'
 require_relative 'capitalize_decorator'
 require_relative 'rental'
+
 class Person < Nameable
   attr_accessor :name, :age
-  attr_reader :id, :rentals
-
-  # @people = [] # Array of Person instances
+  attr_reader :id, :rentals, :parent_permission
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
@@ -15,7 +14,6 @@ class Person < Nameable
     @name = name
     @parent_permission = parent_permission
     @rentals = []
-    # self.class.add_person(self) # Add the instance to the class instance variable
   end
 
   def can_use_services
@@ -26,20 +24,9 @@ class Person < Nameable
     @name
   end
 
-  def add_rental(book, date)
+  def add_rental(date, book)
     Rental.new(date, self, book)
   end
-
-  # def self.add_person(person)
-  #   @people ||= [] # Ensure @people is not nil
-  #   @people.push(person)
-  # end
-
-  # Class method only accessible through the class, not by the instances.
-  # Returns all the instances of the class.
-  # def self.all
-  #   @people
-  # end
 
   private
 
